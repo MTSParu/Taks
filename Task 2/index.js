d066aae3728710e41989ead84ee65e01b1107e1a
M Talha Sarif <mtalhasharif.paru05@gmal.com>
2024-01-12 14:37:11 +0500

    Task2 Completed

--- Task 2/index.js
+++ Task 2/index.js
@@ -14,3 +14,11 @@ btn.addEventListener('click', function onClick() {
 
 var date = new Date();
 document.getElementById("p1").innerHTML = date;
+
+const addBtn = document.getElementById("add-btn")
+addBtn.addEventListener('click', function onClick() {
+  const number1 = document.getElementById("number1").value
+  const number2 = document.getElementById("number2").value
+  const total = parseInt(number1) + parseInt(number2);
+  document.getElementById("total-results").innerHTML = total;
+});
